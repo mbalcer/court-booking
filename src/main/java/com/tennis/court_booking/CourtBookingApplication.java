28b7a82c5e11c7567143df80f68cd7fef07e193b
mbalcer <mateusz75319@gmail.com>
2025-11-15 17:10:02 +0100

    Initialize project

---
+++ src/main/java/com/tennis/court_booking/CourtBookingApplication.java
@@ -0,0 +1,13 @@
+package com.tennis.court_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourtBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourtBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tennis/court_booking/CourtBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tennis.court_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourtBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
